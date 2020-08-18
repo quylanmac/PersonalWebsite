9f535b15bf8e27a23590c18cdc32ff58916cac91
Quy Mac <quylanmac@gmail.com>
2020-08-18 16:12:16 -0700

    Updates

---
+++ precache-manifest.e04492070bb94597977729fbb9d25d5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8213876efd96e212a7414daf6d3d25a",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "6f538d4237ea6b9b6a44",
+    "url": "/reactwebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f15096b675cf49debfa5",
+    "url": "/reactwebsite/static/js/2.d2be5098.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactwebsite/static/js/2.d2be5098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f538d4237ea6b9b6a44",
+    "url": "/reactwebsite/static/js/main.1fabcd9d.chunk.js"
+  },
+  {
+    "revision": "eec87808e158f3f4700a",
+    "url": "/reactwebsite/static/js/runtime-main.824e2a87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
